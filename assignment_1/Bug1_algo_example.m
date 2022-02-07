5450a5f4f404a345614a1df642ae53d5e467f5b2
PlutoMan92 <97783208+PlutoMan92@users.noreply.github.com>
2022-02-07 12:08:05 +0530

    Update and rename Bug1_algo.m to Bug1_algo_example.m
    E1.8(iv)

--- assignment_1/Bug1_algo_example.m
+++ assignment_1/Bug1_algo_example.m
@@ -3,32 +3,38 @@
 clc;
 clear all;
 
-%%
-%input file
+%% % input file 
+% ........................
+% READ ME
+% ........................
+%     1.obstacle polygon vertices are numbered in clock-wise direction
+% 
+%     2.number 1 vertex will be the down most point of the side where the
+%       will collide
 ..................................................................................................................
 s=0.1;  % s-step_size
+t=0.1;  %t-tolerence
 p_start= [0 0];
 p_goal=[5 3];
 obs1=[1 0;1 2;3 0];
 obs2=[4 1;2 3;5 2];
 xboundary=[0 5];
 yboundary=[0 3];
-status='no path exsist'
+status='no path exsist';
 
-%%
-%environment plotting
+%% % environment plotting
 ...............................................................................................................
-x_p=linspace(0,5,0.01);
-y_p=linspace(0,3,0.01);
-
-% plotting the environment
+x_p=linspace(xboundary(1,1),xboundary(1,2),0.01);
+y_p=linspace(yboundary(1,1),yboundary(1,2),0.01);
 plot(x_p,y_p)
+
 xlabel('x-axis')
 xlim(xboundary)
 ylabel('y-axis')
 ylim(yboundary)
 hold on;
 grid on;
+
 
 %plotting p_start and p_goal
 plot(p_goal(1,1),p_goal(1,2),'*')
@@ -75,12 +81,12 @@ y=p_start(1,2);
 distance(1)=sqrt((x-p_goal(1,1))^2-(y-p_goal(1,2))^2);
 theta(1)=atand((x-p_goal(1,2))/(y-p_goal(1,1)));
 
-
 %calculating p_hit1
-A1=[-3 5;1 0]; b1=[0;1];
-p_hit1=A1\b1;
+L1=[x  y;p_goal(1,1) p_goal(1,2)];
+[inter1] =intersect(obs1_plot,L1);  %inter1=phit1
 
-while distance(1) > 0.2 
+
+while distance(1) > s 
 
    x=x+s*cosd(theta(1));
    y=y+s*sind(theta(1));
@@ -89,22 +95,24 @@ while distance(1) > 0.2
    hold on
    drawnow 
 
-   distance(1)=sqrt((x-p_goal(1))^2+(y-p_goal(2))^2);
-   dis_obs(1)=sqrt((x-p_hit1(1))^2+(y-p_hit1(2))^2);
+   distance(1)=sqrt((x-p_goal(1,1))^2+(y-p_goal(1,2))^2);
+   dis_obs(1)=sqrt((x-inter1(1,1))^2+(y-inter1(1,2))^2);
   
-   if dis_obs(1) < 0.15 
+   if dis_obs(1) < t
      %bug1_circumnavigate()
      break
-   elseif x>5 || x<0 || y<0 || y>3
-        status
+   elseif x>xboundary(1,2) || x<xboundary(1,1) || y<yboundary(1,1) || y>yboundary(1,2)
+       text(((xboundary(1,2)-xboundary(1,1))/2)-0.5,(yboundary(1,2)-yboundary(1,1))/2 , status);
    end
 end
+
 
 %%
 %circumnavigate obs1
 ..........................................................................................................
 
-while y < obs1(2,2)
+while   y < obs1(2,2) && y >obs1(1,2) 
+
    plot(x,y,'.')
    hold on
    drawnow 
@@ -114,17 +122,23 @@ while y < obs1(2,2)
   
 end
 
+if x>xboundary(1,2) || x<xboundary(1,1) || y<yboundary(1,1) || y>yboundary(1,2)
+       text(((xboundary(1,2)-xboundary(1,1))/2)-0.5,(yboundary(1,2)-yboundary(1,1))/2 , status);
+      
+end
+
 %%
 %bug1 path after leaving obs1
-%......................................................................................................................
+.....................................................................................................................
 distance(2)=sqrt((x-p_goal(1,1))^2-(y-p_goal(1,2))^2);
 theta(2)=atand((y-p_goal(1,2))/(x-p_goal(1,1)));
 
 %calculating p_hit2
-A2=[-1 5;1 1]; b2=[9;5];
-p_hit2=A2\b2;
+L2=[x  y;p_goal(1,1) p_goal(1,2)];
+[inter2] =intersect(obs2_plot,L2);
 
-while distance(2) > 0.1 
+
+while distance(2) > s 
    plot(x,y,'.')
    hold on
    drawnow 
@@ -133,13 +147,12 @@ while distance(2) > 0.1
    y=y+s*sind(theta(2));
      
    distance(2)=sqrt((x-p_goal(1,1))^2+(y-p_goal(1,2))^2);
-   dis_obs(2)=sqrt((x-p_hit2(1))^2+(y-p_hit2(2))^2);
+   dis_obs(2)=sqrt((x-inter2(1,1))^2+(y-inter2(1,2))^2);
   
-   if dis_obs(2) < 0.2  
-     
-     break
-   elseif x>5 || x<0 || y<0 || y>3
-        status
+   if dis_obs(2) < t  
+        break
+   elseif x>xboundary(1,2) || x<xboundary(1,1) || y<yboundary(1,1) || y>yboundary(1,2)
+       text(((xboundary(1,2)-xboundary(1,1))/2)-0.5,(yboundary(1,2)-yboundary(1,1))/2 , status);
    end
 end
 
@@ -148,7 +161,7 @@ end
 %circumnavigate obs2
 ..........................................................................................................
 
-while y < obs2(2,2)
+while y < obs2(2,2) && y >obs2(1,2)  
 
    x=x+s*cosd(m(2,1));
    y=y+s*sind(m(2,1));
@@ -156,12 +169,41 @@ while y < obs2(2,2)
    plot(x,y,'.')
    hold on
    drawnow 
-
-   if x>5 || x<0 || y<0 || y>3
-    break
-   end
   
 end
 
-status
+if x>xboundary(1,2) || x<xboundary(1,1) || y<yboundary(1,1) || y>yboundary(1,2)
+      text(((xboundary(1,2)-xboundary(1,1))/2)-0.5,(yboundary(1,2)-yboundary(1,1))/2 , status);
+      
+end
+
+%%
+%bug1 path after leaving obs1
+.....................................................................................................................
+distance(3)=sqrt((x-p_goal(1,1))^2-(y-p_goal(1,2))^2);
+theta(3)=atand((y-p_goal(1,2))/(x-p_goal(1,1)));
+
+%calculating p_hit2
+L3=[x  y;p_goal(1,1) p_goal(1,2)];
+[inter3] =intersect(obs2_plot,L2);
+
+
+while distance(2) > s 
+   plot(x,y,'.')
+   hold on
+   drawnow 
+
+   x=x+s*cosd(theta(3));
+   y=y+s*sind(theta(3));
+     
+   distance(3)=sqrt((x-p_goal(1,1))^2+(y-p_goal(1,2))^2);
+   dis_obs(2)=sqrt((x-inter3(1,1))^2+(y-inter3(1,2))^2);
+  
+   if dis_obs(2) < t  
+        break
+   elseif x>xboundary(1,2) || x<xboundary(1,1) || y<yboundary(1,1) || y>yboundary(1,2)
+        text(((xboundary(1,2)-xboundary(1,1))/2)-0.5,(yboundary(1,2)-yboundary(1,1))/2 , status);
+        break
+   end
+end
 

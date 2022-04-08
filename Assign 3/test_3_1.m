a81c30985587e6816dd7f3f766ae111a1efada61
PlutoMan92 <97783208+PlutoMan92@users.noreply.github.com>
2022-04-09 01:14:27 +0530

    Update test_3_1.m

--- Assign 3/test_3_1.m
+++ Assign 3/test_3_1.m
@@ -3,45 +3,132 @@ clear
 close all
 
 %% Environment data
-pStart              = [0 0]               ;% Rabot Coordinate = [x-axis y-axis]
-pGoal               = [5 0]               ;% Target Coordinate = [x-axis y-axis]
-tspan               = 0:0.01:5            ;% time duration
+pStart= [0 0];      % Rabot Coordinate = [x-axis y-axis]
+pGoal = [5 0];      % Target Coordinate = [x-axis y-axis]
+tspan = 0:0.01:5;   % time duration
+vRob  = 0.1;        % Robot velocity along x-axis
 
 %% Obstacle data
-obs = [2 2];% Obstacle location
+obs(1).position=[2 -1]; % Moving obstacle
+obs(2).position=[3 1];  % Moving obstacle
+obs(3).position=[4 0];  % Stationary obstacle
+
+obs(1).velocity=[0 0.1];
+obs(2).velocity=[0 0.1];
+obs(3).velocity=[0 0];
+
 obsRad=0.15/2;
 robRad=0.15/2;
 FinalObsRad=obsRad + robRad;
-vMax=0.15;
 
-%% Environment plot
-hold on
-axis([-2 7 -5 5])
+%% Current positions initiiation
+for i=1:3
+    obs(i).current=obs(i).position;
+end
+robot.current=pStart;
 
-plot(pStart(1),pStart(2),'o','Color','r');
-text(pStart(1)+0.2,pStart(2)+0.2,'Start');
+%% obstacle 1&2 motion matrix
+%obs1.....................................................................
+robot.distance(1)=sqrt((pStart(1)-pGoal(1))^2+(pStart(2)-pGoal(2))^2);
+A(1,1,1)=obs(1).current(1);   % x_position
+A(1,2,1)=obs(1).current(2);   % y_position
+A(1,3,1)=0;                   % Time
 
-plot(pGoal(1),pGoal(2),'o','Color','r');
-text(pGoal(1)+0.2,pGoal(2)+0.2,'Goal');
+for k=2:(2/0.1)+1
+   obs(1).current(1)=obs(1).current(1);
+   obs(1).current(2)=obs(1).current(2)+0.1;
+   A(k,1,1)=obs(1).current(1);    % x position
+   A(k,2,1)=obs(1).current(2);    % y position
+   A(k,3,1)=A(k-1,3)+0.1;         % time
+end
+n=k;
+for k=n+1:n+(2/0.1)
+   obs(1).current(1)=obs(1).current(1);
+   obs(1).current(2)=obs(1).current(2)-0.1;
+   A(k,1,1)=obs(1).current(1);    % x position
+   A(k,2,1)=obs(1).current(2);    % y position
+   A(k,3,1)=A(k-1,3)+0.1;         % time
+end
+n=k;
+for k=n+1:n+(2/0.1)
+   obs(1).current(1)=obs(1).current(1);
+   obs(1).current(2)=obs(1).current(2)+0.1;
+   A(k,1,1)=obs(1).current(1);    % x position
+   A(k,2,1)=obs(1).current(2);    % y position
+   A(k,3,1)=A(k-1,3)+0.1;         % time
+end
+%obs2.........................................................................
+A(1,1,2)=obs(2).current(1);   % x_position
+A(1,2,2)=obs(2).current(2);   % y_position
+A(1,3,2)=0;                   % Time
 
-theta=linspace(0,2*pi);
-xobs=FinalObsRad*cos(theta) + obs(1);
-yobs=FinalObsRad*sin(theta) + obs(2);
-plot(xobs,yobs);
-axis equal
+for k=2:(2/0.1)+1
+   obs(2).current(1)=obs(2).current(1);
+   obs(2).current(2)=obs(2).current(2)-0.1;
+   A(k,1,2)=obs(2).current(1);    % x position
+   A(k,2,2)=obs(2).current(2);    % y position
+   A(k,3,2)=A(k-1,3)+0.1;         % time
+end
+n=k;
+for k=n+1:n+(2/0.1)
+   obs(2).current(1)=obs(2).current(1);
+   obs(2).current(2)=obs(2).current(2)+0.1;
+   A(k,1,2)=obs(2).current(1);    % x position
+   A(k,2,2)=obs(2).current(2);    % y position
+   A(k,3,2)=A(k-1,3)+0.1;         % time
+end
+n=k;
+for k=n+1:n+(2/0.1)
+   obs(2).current(1)=obs(2).current(1);
+   obs(2).current(2)=obs(2).current(2)-0.1;
+   A(k,1,2)=obs(2).current(1);    % x position
+   A(k,2,2)=obs(2).current(2);    % y position
+   A(k,3,2)=A(k-1,3)+0.1;         % time
+end
+%robot.......................................................................
+B(1,1)=robot.current(1);   % x_position
+B(1,2)=robot.current(2);   % y_position
+B(1,3)=0;                   % Time
 
-X0=[0 5 vMax vMax];
-[T, Xsol] = ode45(@EOM, tspan, X0);
-figure
-plot(T, Xsol(:,1))
-xlabel('x axis')
-ylabel('Time')
-title('x vs. t')
+for k=2:(5/0.1)+1
+   robot.current(1)=robot.current(1)+0.1;
+   robot.current(2)=robot.current(2);
+   B(k,1)=robot.current(1);    % x position
+   B(k,2)=robot.current(2);    % y position
+   B(k,3)=B(k-1,3)+0.1;        % time
+end
+%% checking the collision
 
-% plot(Xsol(:,1), Xsol(:,3))
-% xlabel('x-axis')
-% ylabel('y-axis')
-% title('x vs. y')
-
-xlim([0 5])
-
+for i=1:length(B)
+    if B(i,1)-A(i,1,1)<0.3 && B(i,2)-A(i,2,1)<0.3         % checking collision with obs 1
+        B(i,1)=B(i,1);
+        B(i,2)=B(i,2)+0.15;
+    elseif B(i,1)-A(i,1,2)<0.3 && B(i,2)-A(i,2,2)<0.3     % checking collision with obs 2
+        B(i,1)=B(i,1);
+        B(i,2)=B(i,2)+0.15;
+    elseif B(i,1)-obs(3).current(1)<0.3 && B(i,2)-obs(3).current(2)<0.3    % checking collision with obs 3
+        B(i,1)=B(i,1);
+        B(i,2)=B(i,2)+0.15;
+    else
+        B(i,1)=B(i,1);
+        B(i,2)=B(i,2)-0.15;
+    end
+end
+%% plot movement  
+for i=1:length(B) 
+    figure(1)
+    xlim([-1 6])
+    ylim([-1 1])
+    grid on
+    viscircles([A(i,1,1) A(i,2,1)],obsRad)
+    axis equal
+    hold on;
+    viscircles([A(i,1,2) A(i,2,2)],obsRad)
+    axis equal
+    hold on;
+    viscircles([obs(3).position(1) obs(3).position(2)],obsRad)
+    hold on;
+    viscircles([B(i,1) B(i,2)],robRad,'Color','g')
+    hold on;
+    hold off;
+end
